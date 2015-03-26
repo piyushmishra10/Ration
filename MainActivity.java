b11c3efcd54e1beb6daa7d6be352b15f0b3a4d9b
Piyush <piyushmishra10@gmail.com>
2015-03-26 07:49:39 +0400

    Initial commit

---
+++ MainActivity.java
@@ -0,0 +1,77 @@
+package com.fts.ration;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    EditText userName;
+    EditText Pass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        // TODO Auto-generated method stub
+        super.onResume();
+        showDialog();
+    }
+
+    void showDialog() {
+        final Dialog login = new Dialog(MainActivity.this);
+
+        login.setContentView(R.layout.login_dialog);
+
+      //  login.setTitle("Login to Pulse 7");
+        // Init button of login GUI
+        Button btnLogin = (Button) login.findViewById(R.id.btnLogin);
+        Button btnCancel = (Button) login.findViewById(R.id.btnCancel);
+        final EditText txtUsername = (EditText) login.findViewById(R.id.txtUsername);
+        final EditText txtPassword = (EditText) login.findViewById(R.id.txtPassword);
+
+        // Attached listener for login GUI button
+        btnLogin.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (txtUsername.getText().toString().trim().length() > 0 && txtPassword.getText().toString().trim().length() > 0) {
+                    // Validate Your login credential here than display message
+                    Toast.makeText(MainActivity.this,
+                            "Login Sucessful", Toast.LENGTH_LONG).show();
+
+                    // Redirect to dashboard / home screen.
+                    login.dismiss();
+                } else {
+                    Toast.makeText(MainActivity.this,
+                            "Please enter Username and Password", Toast.LENGTH_LONG).show();
+
+                }
+            }
+        });
+        btnCancel.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login.dismiss();
+            }
+        });
+
+        login.show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+}
