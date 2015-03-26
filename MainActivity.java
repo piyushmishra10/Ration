376167b741025d0aed63eb8a8d578f18e769ebf1
Piyush <piyushmishra10@gmail.com>
2015-03-26 08:51:33 +0400

    simple login screen, mainly on 7 inch screen that's why login dialog hard coded

--- MainActivity.java
+++ MainActivity.java
@@ -6,8 +6,10 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -29,10 +31,11 @@ public class MainActivity extends Activity {
 
     void showDialog() {
         final Dialog login = new Dialog(MainActivity.this);
-
+        login.requestWindowFeature(Window.FEATURE_NO_TITLE);
         login.setContentView(R.layout.login_dialog);
+        login.setCanceledOnTouchOutside(false);
 
-      //  login.setTitle("Login to Pulse 7");
+        //  login.setTitle("Login to Pulse 7");
         // Init button of login GUI
         Button btnLogin = (Button) login.findViewById(R.id.btnLogin);
         Button btnCancel = (Button) login.findViewById(R.id.btnCancel);
@@ -43,18 +46,20 @@ public class MainActivity extends Activity {
         btnLogin.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (txtUsername.getText().toString().trim().length() > 0 && txtPassword.getText().toString().trim().length() > 0) {
+                if (txtUsername.getText().toString().trim().length() > 0 && txtPassword.getText().toString().trim().length() > 0 && txtUsername.getText().toString().trim().equalsIgnoreCase("admin") && txtPassword.getText().toString().trim().equalsIgnoreCase("admin")) {
                     // Validate Your login credential here than display message
                     Toast.makeText(MainActivity.this,
-                            "Login Sucessful", Toast.LENGTH_LONG).show();
+                            "Login sucessful", Toast.LENGTH_LONG).show();
 
                     // Redirect to dashboard / home screen.
                     login.dismiss();
-                } else {
+                } else if(txtUsername.getText().toString().trim().length() > 0 && txtPassword.getText().toString().trim().length() > 0){
+                    Toast.makeText(MainActivity.this,
+                            "Please enter Correct Username and Password", Toast.LENGTH_LONG).show();
+
+                }else
                     Toast.makeText(MainActivity.this,
                             "Please enter Username and Password", Toast.LENGTH_LONG).show();
-
-                }
             }
         });
         btnCancel.setOnClickListener(new OnClickListener() {
@@ -65,6 +70,8 @@ public class MainActivity extends Activity {
         });
 
         login.show();
+
+        login.getWindow().setLayout(400, 300);
     }
 
     @Override
